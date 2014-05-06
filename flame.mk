630fe0c3f66f0cee0114506692783b6314946ddb
Bruce Sun <brsun@mozilla.com>
2014-05-06 18:52:42 +0800

    Bug 1006414: Add media_codecs.xml and media_profiles.xml into /system/etc/

--- flame.mk
+++ flame.mk
@@ -1,7 +1,9 @@
 $(call inherit-product, device/qcom/msm8610/msm8610.mk)
 
 PRODUCT_COPY_FILES := \
-    device/qcom/msm8610/audio_policy.conf:system/etc/audio_policy.conf
+    device/qcom/msm8610/audio_policy.conf:system/etc/audio_policy.conf \
+    device/qcom/msm8610/media/media_codecs_8610.xml:system/etc/media_codecs.xml \
+    device/qcom/msm8610/media/media_profiles_8610.xml:system/etc/media_profiles.xml
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
 $(call inherit-product-if-exists, vendor/t2m/flame/flame-vendor-blobs.mk)
