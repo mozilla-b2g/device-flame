63161d4804d2d6b583b30500fe83b939e21d75e1
dlee <dlee@mozilla.com>
2014-05-13 12:20:06 +0800

    Bug 1009377 - [Flame] Use correct libnfc-brcm.conf and libnfc-nxp.conf NFC config file

--- flame.mk
+++ flame.mk
@@ -1,7 +1,9 @@
 $(call inherit-product, device/qcom/msm8610/msm8610.mk)
 
 PRODUCT_COPY_FILES := \
-    device/qcom/msm8610/audio_policy.conf:system/etc/audio_policy.conf
+    device/qcom/msm8610/audio_policy.conf:system/etc/audio_policy.conf \
+    device/t2m/flame/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/t2m/flame/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
 $(call inherit-product-if-exists, vendor/t2m/flame/flame-vendor-blobs.mk)
