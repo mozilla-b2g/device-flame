6b6b0eb370eee7ffc5089043203b3b4760c43100
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2014-06-09 15:08:29 +0200

    Bug 1004195 - Build kernel from sources for Flame

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,6 @@
 include device/qcom/msm8610/BoardConfig.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
 
 TARGET_USES_ION := true
 
@@ -17,9 +16,6 @@ TARGET_PROVIDES_INIT_RC := false
 
 # hack to prevent llvm from building
 BOARD_USE_QCOM_LLVM_CLANG_RS := true
-
-# hack to keep things building
-BOARD_KERNEL_SEPARATED_DT := false
 
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BLUEZ := true
